bfe15f7285864e10b1bc8c6cf91da9c3b75b1f93
wenbuduo <861070016@qq.com>
2023-07-16 20:19:42 +0800

    test

---
+++ src/components/ECharts/index.js
@@ -0,0 +1,6 @@
+import BaseEchart from "./src/BaseEchart.vue";
+import LineEchart from "./src/LineEchart.vue";
+import BarEchart from "./src/BarEchart.vue";
+import PieEchart from "./src/PieEchart.vue";
+
+export { BaseEchart, LineEchart, BarEchart, PieEchart };
